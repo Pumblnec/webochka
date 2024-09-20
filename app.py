5140711a34e452539a0fe9e14ae372ecb3c3ebab
Ivan Varyuhin <ivarkem@gmail.com>
2024-09-20 18:55:24 +0700

    Вывод страницы лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+
+<!doctype html>
+<html>
+    <head>
+        <title>Варюхин Иван Алексеевич.
+        Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ ФБ Лабораторная работа 1
+        </header>
+
+        <h1> web-сервер на flask</h1>
+
+        <footer> 
+            &copy; Варюхин Иван, ФБИ-23, 2024
+        </footer>
+    </body>
+</html>
+
+"""
+
