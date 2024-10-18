4fe2b7834c0bde4b52b47cdcae5889612e84134e
Ivan Varyuhin <ivarkem@gmail.com>
2024-10-18 18:16:54 +0700

    Еще одна заготовка для третьей

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 
 @app.route("/")
@@ -12,42 +14,6 @@ app.register_blueprint(lab2)
 def start():
     return redirect ('/menu', code=302)
 
-@app.route("/lab2/example")
-def example():
-    namee = 'Варюхин Иван'
-    course = '3'
-    group = 'ФБИ-23'
-    labnum = '2'
-    fruits = [
-        {'name': 'Яблоки', 'price': 180},
-        {'name': 'Киви', 'price': 210},
-        {'name': 'Апельсины', 'price': 150},
-        {'name': 'Капуста?', 'price': 80},
-        {'name': 'Манго', 'price': 270}
-        
-    ]
-    books = [
-        {'name': 'Зов Ктулху. Тень над Исмунтом', 'author': 'Лавкрафт Г.', 'genre': 'Ужасы, мистика', 'pages': 336},
-        {'name': 'Ночной дозор', 'author': 'Лукьяненко С.', 'genre': 'Роман, фантастика, ужасы', 'pages': 410},
-        {'name': 'Легкий способ бросить курить', 'author': 'Карр А.', 'genre': 'Методики, психология', 'pages': 224},
-        {'name': 'Билли Саммерс', 'author': 'Кинг С.', 'genre': 'Детектив, триллер', 'pages': 550},
-        {'name': 'Зеленая миля', 'author': 'Кинг С.', 'genre': 'Психологический триллер', 'pages': 384},
-        {'name': 'ТЕКСТ', 'author': 'Глуховский Д.', 'genre': 'Криминальная драма, психологический триллер', 'pages': 330},
-        {'name': 'Снеговик', 'author': 'Несбё Ю.', 'genre': 'Криминальная драма, детектив', 'pages': 460},
-        {'name': 'Нетопырь', 'author': 'Несбё Ю.', 'genre': 'Криминальная драма, детектив', 'pages': 330},
-        {'name': 'Ядовитый воздух свободы', 'author': 'Блейк А.', 'genre': 'Психологические детективы', 'pages': 473},
-        {'name': 'Я прихожу после смерти. История уборщика мест преступлений', 'author': 'Кундт Т., Багджи Т.', 'genre': 'Трукрайм', 'pages': 170}
-    ]
-    return render_template ('example.html', namee=namee, course=course, group=group, labnum=labnum, fruits = fruits, books=books)
-
-@app.route("/lab2/")
-def lab2():
-
-    return  render_template ('lab2.html')
-
-@app.route("/lab2/vector")
-def vector():
-    return render_template ('vector.html')
 
 @app.route("/menu")
 def mn():
@@ -76,6 +42,8 @@ def mn():
             <li><a href= 'http://127.0.0.1:5000/lab1'> Лабораторная 1 </a></li>
 
             <li><a href= 'http://127.0.0.1:5000/lab2'> Лабораторная 2 </a></li>
+
+            <li><a href= 'http://127.0.0.1:5000/lab3'> Лабораторная 3</a></li>
 
         </ol>
 
