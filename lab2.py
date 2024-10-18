68a66292f20728d075cb9e6a1c0eb1c7ba786575
Ivan Varyuhin <ivarkem@gmail.com>
2024-10-18 17:25:17 +0700

    Рефакторинг 2-ой лабы

--- lab2.py
+++ lab2.py
@@ -1,18 +1,8 @@
-from flask import Flask, redirect, url_for, render_template
-from lab1 import lab1
-from lab2 import lab2
-
-app = Flask(__name__)
-app.register_blueprint(lab1)
-app.register_blueprint(lab2)
+from flask import Blueprint, redirect, url_for, render_template
+lab2 = Blueprint('lab2', __name__)
 
 
-@app.route("/")
-@app.route("/index")
-def start():
-    return redirect ('/menu', code=302)
-
-@app.route("/lab2/example")
+@lab2.route("/lab2/example")
 def example():
     namee = 'Варюхин Иван'
     course = '3'
@@ -40,52 +30,11 @@ def example():
     ]
     return render_template ('example.html', namee=namee, course=course, group=group, labnum=labnum, fruits = fruits, books=books)
 
-@app.route("/lab2/")
+@lab2.route("/lab2/")
 def lab2():
 
     return  render_template ('lab2.html')
 
-@app.route("/lab2/vector")
+@lab2.route("/lab2/vector")
 def vector():
     return render_template ('vector.html')
-
-@app.route("/menu")
-def mn():
-    return """
-
-<!doctype html>
-<html>
-    <head>
-        <title>Варюхин Иван Алексеевич.
-        Лабораторная 1</title>
-        <link rel="stylesheet" href='""" + url_for('static', filename='lab1.css') + """'>
-    </head>
-    <body>
-        <header>
-            НГТУ ФБ WEB-программирование, часть 2. Список лабораторных
-        </header>
-
-        
-        <h1> web-сервер на flask</h1>
-        <main> 
-
-        <h2>Меню</h2>
-
-        <ol>
-
-            <li><a href= 'http://127.0.0.1:5000/lab1'> Лабораторная 1 </a></li>
-
-            <li><a href= 'http://127.0.0.1:5000/lab2'> Лабораторная 2 </a></li>
-
-        </ol>
-
-        </main>
-
-
-        <footer> 
-            &copy; Варюхин Иван, ФБИ-23, 3 курс, 2024
-        </footer>
-    </body>
-</html>
-
-"""
