5174784c25f2d663a0046cf271072a6265d9085f
Ivan Varyuhin <ivarkem@gmail.com>
2024-12-11 17:55:53 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -7,6 +7,7 @@ def labiche():
     name_color = request.cookies.get('name_color')
     return render_template ('lab3/lab3.html', name=name,  name_color=name_color)
 
+
 @lab3.route('/lab3/cookie')
 def luv():
     resp = make_response(redirect('/lab3/'))
@@ -15,6 +16,7 @@ def luv():
     resp.set_cookie('name_color','DarkSlateBlue')
     return resp
 
+
 @lab3.route('/lab3/del_cookie')
 def delete():
     resp = make_response(redirect('/lab3/'))
@@ -22,6 +24,7 @@ def delete():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
 
 @lab3.route('/lab3/form1')
 def form():
@@ -36,4 +39,32 @@ def form():
         errors['age'] = '* заполните поле!'
     sex = request.args.get('sex')
     return render_template('/lab3/form1.html', user = user, sex=sex, age=age, errors = errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'coffe':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price', 0)
+    return render_template('lab3/success.html', price=price)
 
