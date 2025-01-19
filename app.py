926cebb84f204da38ab491fd9ac7833139b5f5c6
Ivan Varyuhin <ivarkem@gmail.com>
2025-01-19 22:03:44 +0700

    Итоги лаб 7

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 app = Flask(__name__)
 app.config['DB_TYPE'] = 'postgres'
@@ -17,6 +18,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 
 @app.route("/")
@@ -60,6 +62,8 @@ def mn():
             <li><a href= '/lab5'> Лабораторная 5 </a></li>
 
             <li><a href= '/lab6'> Лабораторная 6 </a></li>
+
+            <li><a href= '/lab7'> Лабораторная 7 </a></li>
 
         </ol>
 
